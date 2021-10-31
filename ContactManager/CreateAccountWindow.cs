e6d536f4ff55459a107f8a3dcc3d2f0d99040d39
Ghatsy_ <franc.maytas@gmail.com>
2021-10-31 15:51:21 +0100

    Přidejte soubory projektu.

---
+++ ContactManager/CreateAccountWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactManager
+{
+    public partial class CreateAccountWindow : Form
+    {
+        public CreateAccountWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelAccount_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
---
+++ ContactManager/LoginWindow.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace ContactManager
+{
+    partial class LoginWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginLogin = new System.Windows.Forms.Button();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.cancelLogin = new System.Windows.Forms.Button();
+            this.createAccount = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.showPasswordCheckBox = new System.Windows.Forms.CheckBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loginLogin
+            // 
+            this.loginLogin.Location = new System.Drawing.Point(126, 103);
+            this.loginLogin.Name = "loginLogin";
+            this.loginLogin.Size = new System.Drawing.Size(108, 23);
+            this.loginLogin.TabIndex = 0;
+            this.loginLogin.Text = "Login";
+            this.loginLogin.UseVisualStyleBackColor = true;
+            this.loginLogin.Click += new System.EventHandler(this.loginLogin_Click);
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(78, 22);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(156, 23);
+            this.usernameTextBox.TabIndex = 1;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(78, 51);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(156, 23);
+            this.passwordTextBox.TabIndex = 2;
+            // 
+            // cancelLogin
+            // 
+            this.cancelLogin.Location = new System.Drawing.Point(12, 103);
+            this.cancelLogin.Name = "cancelLogin";
+            this.cancelLogin.Size = new System.Drawing.Size(108, 23);
+            this.cancelLogin.TabIndex = 3;
+            this.cancelLogin.Text = "Cancel";
+            this.cancelLogin.UseVisualStyleBackColor = true;
+            this.cancelLogin.Click += new System.EventHandler(this.cancelLogin_Click);
+            // 
+            // createAccount
+            // 
+            this.createAccount.Location = new System.Drawing.Point(24, 150);
+            this.createAccount.Name = "createAccount";
+            this.createAccount.Size = new System.Drawing.Size(222, 23);
+            this.createAccount.TabIndex = 4;
+            this.createAccount.Text = "Create account";
+            this.createAccount.UseVisualStyleBackColor = true;
+            this.createAccount.Click += new System.EventHandler(this.createAccount_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.showPasswordCheckBox);
+            this.groupBox1.Controls.Add(this.passwordLabel);
+            this.groupBox1.Controls.Add(this.usernameLabel);
+            this.groupBox1.Controls.Add(this.usernameTextBox);
+            this.groupBox1.Controls.Add(this.passwordTextBox);
+            this.groupBox1.Controls.Add(this.cancelLogin);
+            this.groupBox1.Controls.Add(this.loginLogin);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox1.Size = new System.Drawing.Size(247, 132);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
+            // 
+            // showPasswordCheckBox
+            // 
+            this.showPasswordCheckBox.AutoSize = true;
+            this.showPasswordCheckBox.Location = new System.Drawing.Point(105, 78);
+            this.showPasswordCheckBox.Name = "showPasswordCheckBox";
+            this.showPasswordCheckBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.showPasswordCheckBox.Size = new System.Drawing.Size(108, 19);
+            this.showPasswordCheckBox.TabIndex = 7;
+            this.showPasswordCheckBox.Text = "Show password";
+            this.showPasswordCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(12, 54);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(60, 15);
+            this.passwordLabel.TabIndex = 5;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(12, 25);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(63, 15);
+            this.usernameLabel.TabIndex = 4;
+            this.usernameLabel.Text = "Username:";
+            // 
+            // LoginWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 179);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.createAccount);
+            this.Name = "LoginWindow";
+            this.Text = "Contact Manager";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginLogin;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button cancelLogin;
+        private System.Windows.Forms.Button createAccount;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.CheckBox showPasswordCheckBox;
+    }
+}
+
